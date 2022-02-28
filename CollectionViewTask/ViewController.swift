0f529950f6e9819519ea3d55f7b8e856fcad9c3b
Ksenia <ms.ksi2000@mail.ru>
2022-02-28 16:26:56 +0300

    Initial Commit

---
+++ CollectionViewTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CollectionViewTask
+//
+//  Created by Kseniya on 28.02.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
